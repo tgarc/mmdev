3c6c01147d6beed7ab1105fcbf3eae74f8501016
Thomas Garcia <toemossgarcia@gmail.com>
2015-11-05 17:19:20 -0600

    simplified regdegs; lowercased attributes

--- armv7m.py
+++ armv7m.py
@@ -28,14 +28,14 @@ BLK_MAP = \
 
 MEM_MAP = \
 {
-    'TPIU'      : (0xE0040000, 0xE0041000)
-  , 'ETM'       : (0xE0041000, 0xE0042000)
-  , 'EPPB'      : (0xE0042000, 0xE00FF000)
-  , 'ROM_TABLE' : (0xE00FF000, 0xE00FFFFF)
-  , 'ITM'       : (0xE0000000, 0xE0001000)
-  , 'DWT'       : (0xE0001000, 0xE0002000)
-  , 'FPB'       : (0xE0002000, 0xE0003000)
-  , 'SCS'       : (0xE000E000, 0xE000F000)
+    'TPIU'      : 0xE0040000
+  , 'ETM'       : 0xE0041000
+  , 'EPPB'      : 0xE0042000
+  , 'ROM_TABLE' : 0xE00FF000
+  , 'ITM'       : 0xE0000000
+  , 'DWT'       : 0xE0001000
+  , 'FPB'       : 0xE0002000
+  , 'SCS'       : 0xE000E000
 }
 
 REG_MAP = \
@@ -51,62 +51,62 @@ REG_MAP = \
 BIT_MAP = \
 {
     'DEMCR' : {
-        'TRCENA'       : (0x01000000, 24)
-      , 'MON_REQ'      : (0x00080000, 19)
-      , 'MON_STEP'     : (0x00040000, 18)
-      , 'MON_PEND'     : (0x00020000, 17)
-      , 'MON_EN'       : (0x00010000, 16)
-      , 'VC_HARDERR'   : (0x00000400, 10)
-      , 'VC_INTERR'    : (0x00000200,  9)
-      , 'VC_BUSERR'    : (0x00000100,  8)
-      , 'VC_STATERR'   : (0x00000080,  7)
-      , 'VC_CHKERR'    : (0x00000040,  6)
-      , 'VC_NOCPERR'   : (0x00000020,  5)
-      , 'VC_MMERR'     : (0x00000010,  4)
-      , 'VC_CORERESET' : (0x00000001,  0)
+        'TRCENA'       : 0x01000000
+      , 'MON_REQ'      : 0x00080000
+      , 'MON_STEP'     : 0x00040000
+      , 'MON_PEND'     : 0x00020000
+      , 'MON_EN'       : 0x00010000
+      , 'VC_HARDERR'   : 0x00000400
+      , 'VC_INTERR'    : 0x00000200
+      , 'VC_BUSERR'    : 0x00000100
+      , 'VC_STATERR'   : 0x00000080
+      , 'VC_CHKERR'    : 0x00000040
+      , 'VC_NOCPERR'   : 0x00000020
+      , 'VC_MMERR'     : 0x00000010
+      , 'VC_CORERESET' : 0x00000001
   }
   , 'DFSR': {
-        'EXTERNAL' : (0x00000010, 4)
-      , 'VCATCH'   : (0x00000008, 3)
-      , 'DWTTRAP'  : (0x00000004, 2)
-      , 'BKPT'     : (0x00000002, 1)
-      , 'HALTED'   : (0x00000001, 0)
+        'EXTERNAL' : 0x00000010
+      , 'VCATCH'   : 0x00000008
+      , 'DWTTRAP'  : 0x00000004
+      , 'BKPT'     : 0x00000002
+      , 'HALTED'   : 0x00000001
   }
   , 'DCRSR':  {
-        'REGWnR': (0x00010000, 16)
-      , 'REGSEL': (0x0008001F,  0)
+        'REGWnR': 0x00010000
+      , 'REGSEL': 0x0008001F
   }
   , 'DCRDR':  {
-        'DBGTMP': (0xFFFFFFFF, 0)
+        'DBGTMP': 0xFFFFFFFF
   }
   , 'DHCSR':  {
-        'S_RESET_ST'  : (0x02000000, 25)
-      , 'S_RETIRE_ST' : (0x01000000, 24)
-      , 'S_LOCKUP'    : (0x00080000, 19)
-      , 'S_SLEEP'     : (0x00040000, 18)
-      , 'S_HALT'      : (0x00020000, 17)
-      , 'S_REGRDY'    : (0x00010000, 16)
-      , 'C_SNAPSTALL' : (0x00000020,  5)
-      , 'C_MASKINTS'  : (0x00000008,  3)
-      , 'C_STEP'      : (0x00000004,  2)
-      , 'C_HALT'      : (0x00000002,  1)
-      , 'C_DEBUGEN'   : (0x00000001,  0)
+        'S_RESET_ST'  : 0x02000000
+      , 'S_RETIRE_ST' : 0x01000000
+      , 'S_LOCKUP'    : 0x00080000
+      , 'S_SLEEP'     : 0x00040000
+      , 'S_HALT'      : 0x00020000
+      , 'S_REGRDY'    : 0x00010000
+      , 'C_SNAPSTALL' : 0x00000020
+      , 'C_MASKINTS'  : 0x00000008
+      , 'C_STEP'      : 0x00000004
+      , 'C_HALT'      : 0x00000002
+      , 'C_DEBUGEN'   : 0x00000001
   }
   , 'AIRCR':   { 
-        'ENDIANNESS'    : (0x00008000, 15)
-      , 'PRIGROUP'      : (0x00000700,  8)
-      , 'SYSRESETREQ'   : (0x00000004,  2)
-      , 'VECTCLRACTIVE' : (0x00000002,  1)
-      , 'VECTRESET'     : (0x00000001,  0)
+        'ENDIANNESS'    : 0x00008000
+      , 'PRIGROUP'      : 0x00000700
+      , 'SYSRESETREQ'   : 0x00000004
+      , 'VECTCLRACTIVE' : 0x00000002
+      , 'VECTRESET'     : 0x00000001
   }
   , 'ITM_TCR': {
-        'BUSY'       : (0x00800000, 23)
-      , 'TraceBusID' : (0x007F0000, 16)
-      , 'TSPrescale' : (0x00000300,  8)
-      , 'SWOENA'     : (0x00000010,  4)
-      , 'TXENA'      : (0x00000008,  3)
-      , 'SYNCENA'    : (0x00000004,  2)
-      , 'TSENA'      : (0x00000002,  1)
-      , 'ITMENA'     : (0x00000001,  0)
+        'BUSY'       : 0x00800000
+      , 'TraceBusID' : 0x007F0000
+      , 'TSPrescale' : 0x00000300
+      , 'SWOENA'     : 0x00000010
+      , 'TXENA'      : 0x00000008
+      , 'SYNCENA'    : 0x00000004
+      , 'TSENA'      : 0x00000002
+      , 'ITMENA'     : 0x00000001
   }
 }
--- mmdev.py
+++ mmdev.py
@@ -1,4 +1,4 @@
-getattrd = lambda obj,x: reduce(getattr,x.split('.'),obj)
+getattrd = lambda obj, x1, x2: getattr(getattr(obj, x1), x2)
 
 
 class DUT:
@@ -8,15 +8,14 @@ class DUT:
 
         self._mmap = dutdef.MEM_MAP.keys()
         for k in self._mmap:
-            setattr(self,k,Block(dutdef, k))
-            setattr(self,k.lower(),getattr(self,k))
-        self._mmap = sorted(self._mmap, key=lambda k: getattrd(self,k+'.addr'), reverse=True)
+            setattr(self, k.lower(), Block(dutdef, k))
+        self._mmap = sorted(self._mmap, key=lambda k: getattrd(self, k.lower(), 'addr'), reverse=True)
 
         self._linefmt = "0x{:08X} {:s}".format
 
     def __repr__(self):
         dstr = "{:s}\n".format(self.name)
-        dstr+= "Blocks:\n\t" + "\n\t".join([self._linefmt(getattrd(self,k+'.addr'),k) for k in self._mmap])
+        dstr+= "Blocks:\n\t" + "\n\t".join([self._linefmt(getattrd(self, k.lower(), 'addr'), k) for k in self._mmap])
         return dstr
 
     def __str__(self):
@@ -26,26 +25,25 @@ class DUT:
 class Block:
     def __init__(self, dutdef, blockname):
         self.mnemonic = blockname
-        self.addr = dutdef.MEM_MAP[blockname][0]
+        self.addr = dutdef.MEM_MAP[blockname]
 
-        self.name = dutdef.BLK_NAME.get(self.mnemonic,'block')
-        self.descr = dutdef.BLK_DESCR.get(self.mnemonic,'')
+        self.name = dutdef.BLK_NAME.get(self.mnemonic, 'block')
+        self.descr = dutdef.BLK_DESCR.get(self.mnemonic, '')
 
-        self._rmap = dutdef.BLK_MAP.get(self.mnemonic,())
+        self._rmap = dutdef.BLK_MAP.get(self.mnemonic, ())
         for k in self._rmap:
-            setattr(self,k,Register(dutdef, k))
-            setattr(self,k.lower(),getattr(self,k))            
-        self._rmap = sorted(self._rmap, key=lambda k: getattrd(self,k+'.addr'), reverse=True)
+            setattr(self, k.lower(), Register(dutdef, k))
+        self._rmap = sorted(self._rmap, key=lambda k: getattrd(self, k.lower(), 'addr'), reverse=True)
 
         self._linefmt = "0x{:08X} {:s}".format
 
     def __repr__(self):
-        dstr = "{:s} ({:s}, 0x{:08X})\n".format(self.name,self.mnemonic,self.addr)
-        dstr+= "Registers:\n\t" + "\n\t".join([self._linefmt(getattrd(self,k+'.addr'),k) for k in self._rmap])
+        dstr = "{:s} ({:s}, 0x{:08X})\n".format(self.name, self.mnemonic, self.addr)
+        dstr+= "Registers:\n\t" + "\n\t".join([self._linefmt(getattrd(self, k.lower(), 'addr'), k) for k in self._rmap])
         return dstr
 
     def __str__(self):
-        return "block('{:s}', 0x{:08X})".format(self.mnemonic,self.addr)
+        return "block('{:s}', 0x{:08X})".format(self.mnemonic, self.addr)
 
 
 class Register:
@@ -53,30 +51,29 @@ class Register:
         self.mnemonic = regname
         self.addr = dutdef.REG_MAP[self.mnemonic]
 
-        self.name = dutdef.REG_NAME.get(self.mnemonic,'register')
-        self.descr = dutdef.REG_DESCR.get(self.mnemonic,'')
+        self.name = dutdef.REG_NAME.get(self.mnemonic, 'register')
+        self.descr = dutdef.REG_DESCR.get(self.mnemonic, '')
 
-        self._bmap = dutdef.BIT_MAP.get(self.mnemonic,())
+        self._bmap = dutdef.BIT_MAP.get(self.mnemonic, ())
         for k in self._bmap:
-            setattr(self, k, BitField(self.mnemonic,k))
-            setattr(self,k.lower(),getattr(self,k))            
-        self._bmap = sorted(self._bmap, key=lambda k: getattrd(self,k+'.offset'), reverse=True)
+            setattr(self, k.lower(), BitField(dutdef, self.mnemonic, k))
+        self._bmap = sorted(self._bmap, key=lambda k: getattrd(self, k.lower(), 'mask'), reverse=True)
 
         self._linefmt = "0x{:08X} {:s}".format
 
     def __repr__(self):
-        dstr = "{:s} ({:s}, 0x{:08X})\n".format(self.name,self.mnemonic,self.addr)
-        dstr+= "BitFields:\n\t" + "\n\t".join([self._linefmt(getattrd(self,k+'.mask'),k) for k in self._bmap])
+        dstr = "{:s} ({:s}, 0x{:08X})\n".format(self.name, self.mnemonic, self.addr)
+        dstr+= "BitFields:\n\t" + "\n\t".join([self._linefmt(getattrd(self, k.lower(), 'mask'), k) for k in self._bmap])
         return dstr
 
     def __str__(self):
-        return "register('{:s}', 0x{:08X})".format(self.mnemonic,self.addr)
+        return "register('{:s}', 0x{:08X})".format(self.mnemonic, self.addr)
 
 class BitField:
-    def __init__(self, regname, bfname):
+    def __init__(self, dutdef, regname, bfname):
         self.mnemonic = bfname
         self.register = regname
-        self.mask, self.offset = BIT_MAP[self.register][self.mnemonic]
+        self.mask = dutdef.BIT_MAP[self.register][self.mnemonic]
 
     def __repr__(self):
         return "bitfield('{:s}', 0x{:08X})".format(self.mnemonic, self.mask)
